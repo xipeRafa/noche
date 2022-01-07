5788b3df42b939dec1d71bc1515394dcc19f28c5
rafael <rafaelsistem87@gmail.com>
2022-01-07 02:03:40 -0700

    colores

--- src/components/auctions/AuctionBody.js
+++ src/components/auctions/AuctionBody.js
@@ -6,7 +6,7 @@ import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ItemSelected from "./ItemSelected";
 import { useFirestore } from '../../hooks/useFirestore';
-
+import List from "./List";
 import "./picker.css";
 import es from "date-fns/locale/es";
 
@@ -56,8 +56,6 @@ export const AuctionBody = () => {
   const [fecha, setFecha] = useState();
   const [arr, setArr] = useState([]);
 
-  console.log(arr.filter(el => el !== undefined))
-
   let l = arr.filter((el) => el).length;
 
   const onDate = (fecha) => {
@@ -96,14 +94,10 @@ export const AuctionBody = () => {
     setArrRadio([]);
   };
 
-  const [arr2, setArr2] = useState([]);
-
   const [n, setN] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    setArr2(mail);
 
     let sss = arr
       .filter((el) => el !== undefined)
@@ -114,194 +108,7 @@ export const AuctionBody = () => {
 
   /* ===================================== filter Mail END==================== */
 
-  /* ===================================== Radio Filter ==================== */
-  const [radio, setRadio] = useState();
 
-  const [arrRadio, setArrRadio] = useState([]);
-
-  const handlerRadio = (v) => {
-    setRadio(v);
-    setArrRadio([]);
-    const toRadio = n?.filter((el) => el !== false);
-    let r = toRadio;
-
-    if (radio === "completados") {
-      r = toRadio.filter((el) => el.completed !== true);
-      setArrRadio(r);
-    } else {
-      r = toRadio.filter((el) => el.completed !== false);
-      setArrRadio(r);
-    }
-  };
-  /* ===================================== Radio Filter END ==================== */
-
-  /* ===================================== Clientes Filter ==================== */
-
-  let db = n?.sort((o1, o2) => o1.duration - o2.duration) //last to near
-    
-
-  const [oxxo, setoxxo] = useState(true);
-  const [otro, setotro] = useState(true);
-  const [pagoEnEfectivo, setPagoEnEfectivo] = useState(true);
-  const [taller, setTaller] = useState(true);
-
-  const handleoxxo = (e) => {
-    setoxxo(!oxxo);
-    oxxo
-      ? setArrFilter([...arrFilter, e.target.value])
-      : removeItemFromArr(arrFilter, e.target.value);
-  };
-
-  const handleotro = (e) => {
-    setotro(!otro);
-    otro
-      ? setArrFilter([...arrFilter, e.target.value])
-      : removeItemFromArr(arrFilter, e.target.value);
-  };
-
-  const handleEfectivo = (e) => {
-    setPagoEnEfectivo(!pagoEnEfectivo);
-    pagoEnEfectivo
-      ? setArrFilter([...arrFilter, e.target.value])
-      : removeItemFromArr(arrFilter, e.target.value);
-  };
-
-  const handleTaller = (e) => {
-    setTaller(!taller);
-    taller
-      ? setArrFilter([...arrFilter, e.target.value])
-      : removeItemFromArr(arrFilter, e.target.value);
-  };
-
-  const [arrFilter, setArrFilter] = useState([]);
-
-  function removeItemFromArr(arrFilter, item) {
-    let index = arrFilter.indexOf(item);
-    if (index > -1) {
-      arrFilter.splice(index, 1);
-    }
-  }
-
-  useEffect(() => {
-    for (let index = 0; index < arrFilter.length; index++) {
-      const element = db.filter((el) => el.categorie !== arrFilter[index]);
-      db = element;
-    }
-    setArrRadio(db);
-  }, [arrFilter, oxxo, otro, pagoEnEfectivo, taller]);
-
-  /* ===================================== Clientes Filter END ==================== */
-
-
-  /* ===================================== last 24 hours ==================== */
- /*  let lastWeek  = Date.now()-86400000 * 7  */
-  let lastDay = Date.now() - 86400000;
-  let lastHour = Date.now() - 3600000;
-  let lastMinute = Date.now() - 60000;
-
-  const [min, setMin] = useState();
-
-  const [f, setF] = useState(1);
-
-  const handlerInputM = (e) => {
-    setF(e.target.value);
-  };
-
-  let r = 60000 * f;
-
-  const t2 = DBD
-    .filter(el => el.tiendaOxxo)
-    .filter((el) => el !== undefined)
-
-  useEffect(() => {
-
-    let personasMap2 = t2.filter((el) => el.duration > lastMinute - r).map((item) => {
-      return [item.email, item];
-    });
-    let personasMapArr2 = new Map(personasMap2);
-
-    let unicos2 = [...personasMapArr2.values()];
-    setMin(unicos2);
-  }, [f]);
-
-  const handlerMinute = () => {
-
-    let personasMap2 = t2.filter((el) => el.duration > lastMinute - r).map((item) => {
-      return [item.email, item];
-    });
-    let personasMapArr2 = new Map(personasMap2);
-
-    let unicos2 = [...personasMapArr2.values()];
-    setMin(unicos2);
-    setD([]);
-    setH([]);
-  };
-
-  const [d, setD] = useState();
-
-  const handlerDay = () => {
-
-    let personasMap = t2.filter((el) => el.duration > lastDay).map((item) => {
-      return [item.email, item];
-    });
-    let personasMapArr = new Map(personasMap); // Pares de clave y valor
-
-    let unicos = [...personasMapArr.values()]; // Conversión a un array
-    setD(unicos);
-    setMin([]);
-    setH([]);
-  };
-
-  const [h, setH] = useState();
-
-  const [hr, setHr] = useState(1);
-
-  const handlerInputH = (e) => {
-    setHr(e.target.value);
-  };
-
-  let rr = 3600000 * hr;
-
-  useEffect(() => {
-
-    let personasMap1 = t2.filter((el) => el.duration > lastHour - rr).map((item) => {
-      return [item.email, item];
-    });
-    let personasMapArr1 = new Map(personasMap1); // Pares de clave y valor
-
-    let unicos1 = [...personasMapArr1.values()]; // Conversión a un array
-    setH(unicos1);
-  }, [hr]);
-
-  const handlerHour = () => {
-
-    let personasMap1 = t2.filter((el) => el.duration > lastHour).map((item) => {
-      return [item.email, item];
-    });
-    let personasMapArr1 = new Map(personasMap1); // Pares de clave y valor
-
-    let unicos1 = [...personasMapArr1.values()]; // Conversión a un array
-    setH(unicos1);
-    setMin([]);
-    setD([]);
-  };
-
-  /* ===================================== last 24 hours END ==================== */
-
-  let arr3 = arr;
-
-  if (n) {
-    let nn = n.filter((el) => el !== false);
-    arr3 = nn;
-    if (arrRadio.length > 0) {
-      arr3 = arrRadio.filter((el) => el !== false);
-    }
-  } else {
-    if (arr) {
-      arr3 = arr
-        .sort((o1, o2) => o1.duration - o2.duration) //last to near
-    }
-  }
 
   let arr4 = DBD
   
@@ -332,6 +139,10 @@ export const AuctionBody = () => {
 
   const [bool, setBool]=useState(true)
 
+  /* let tiendas = arr.map(el => el?.tiendaOxxo).sort().filter(el => el !== undefined) */
+
+
+/* 
   let personasMap22 = arr.map((item) => {
     return [item?.tiendaOxxo, item];
   });
@@ -339,7 +150,15 @@ export const AuctionBody = () => {
 
   let unicos = [...personasMapArr22.values()]
 
-  console.log(unicos.filter(el => el !== undefined))
+  console.log(unicos.filter(el => el !== undefined)) */
+     
+
+ 
+
+  
+
+
+
 
   return (
     <div className="container-fluid">
@@ -401,198 +220,9 @@ export const AuctionBody = () => {
               />
             </form>
           </div>
-          <div className="col-1"></div>
-          <div className={arr?.length > 0 ? "col-md-3 mt-2" : "d-none"}>
-            <div
-              className={n?.length > 0 ? "w-75 fr" : "d-none"}
-              onChange={(e) => handlerRadio(e.target.value)}
-            >
-              <input
-                type="button"
-                className="btn text-white btn-outline-dark"
-                name="drone"
-                value="Todos"
-                onClick={() => setArrRadio([])}
-              />
-
-              <label className="btn text-white btn-outline-dark my-2">
-                <input
-                  type="radio"
-                  value="completados"
-                  name="drone"
-                  className="d-none"
-                />
-                Completados
-              </label>
-              <label className="btn text-white btn-outline-dark">
-                <input
-                  type="radio"
-                  value="nocompletados"
-                  name="drone"
-                  className="d-none"
-                />
-                Incompletos
-              </label>
-            </div>
-          </div>
-          <div className={n?.length > 0 ? "col-md-3 mt-2" : "d-none"}>
-            <div className="w-75 mt-0 " style={{ marginLeft: "12.5%" }}>
-              <label className="text-white mb-2 mt-1">
-                <input
-                  type="checkbox"
-                  className="m-1"
-                  value="oxxo"
-                  onChange={(e) => handleoxxo(e)}
-                  checked={oxxo}
-                />
-                OXXO
-              </label>
-              <br />
-              <label className="text-white mb-2">
-                <input
-                  type="checkbox"
-                  className="m-1"
-                  value="otro"
-                  onChange={(e) => handleotro(e)}
-                  checked={otro}
-                />
-                Otro
-              </label>
-              <br />
-              <label className="text-white">
-                <input
-                  type="checkbox"
-                  className="m-1 mb-3"
-                  value="pago en efectivo"
-                  onChange={(e) => handleEfectivo(e)}
-                  checked={pagoEnEfectivo}
-                />
-                Pagos en Efectivo
-              </label>
-              <br />
-              <label className="text-white">
-                <input
-                  type="checkbox"
-                  className="m-1 mb-4"
-                  value="taller"
-                  onChange={(e) => handleTaller(e)}
-                  checked={taller}
-                />
-                Taller
-              </label>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* =================================================================================================================================================================================================================================== */}
-      {currentUser && (
-<div className="d-none">
-      <div className="d-flex flex-row justify-content-evenly p-4">
-        <input
-          type="button"
-          className={arr?.length > 0 ? "d-none" : "btn btn-primary"}
-          value="Ultimo Minuto"
-          onClick={handlerMinute}
-        />
-
-        <input
-          type="button"
-          className={arr?.length > 0 ? "d-none" : "btn btn-primary mx-1"}
-          value="Ultima Hora"
-          onClick={handlerHour}
-        />
-
-        <input
-          type="button"
-          className={arr?.length > 0 ? "d-none" : "btn btn-primary"}
-          value="Ultimas 24 Hrs"
-          onClick={handlerDay}
-        />
-      </div>
-
-      <div
-        className={
-          arr?.length > 0 ? "d-none" : min?.length > 0 ? "p-1 mb-3" : "d-none"
-        }
-      >
-        <h4 className="p-1 bg-secondary text-white">
-          Taxistas con Viajes sin Completar desde el Ultimo Minuto +
-          <input
-            type="number"
-            className="mx-5"
-            onChange={handlerInputM}
-            value={f}
-          />
-        </h4>
-
-        {
-          <div className="p-3 text-center mb-3">
-            {min?.map((doc) => {
-              return (
-                <span className="border border-danger mx-2 p-2 bg-white">
-                  {doc.email.slice(0, -10)}
-                </span>
-              );
-            })}
-          </div>
-        }
-      </div>
-
-      <div
-        className={
-          arr?.length > 0 ? "d-none" : h?.length > 0 ? "p-1 mb-3" : "d-none"
-        }
-      >
-        <h4 className="p-1 bg-secondary text-white">
-          Taxistas con Viajes sin Completar de la Ultima Hora +
-          <input
-            type="number"
-            className="mx-5"
-            onChange={handlerInputH}
-            value={hr}
-          />
-        </h4>
-
-        {
-          <div className="p-3 text-center mb-3">
-            {h?.map((doc) => {
-              return (
-                <span className="border border-danger mx-2 p-2 bg-white">
-                  {doc.email.slice(0, -10)}
-                </span>
-              );
-            })}
-          </div>
-        }
-      </div>
-
-      <div
-        className={
-          arr?.length > 0 ? "d-none" : d?.length > 0 ? "p-1 mb-3" : "d-none"
-        }
-      >
-        <h4 className="p-2 bg-secondary text-white">
-          Taxistas con viajes sin Completar de las Ultimas 24 Horas
-        </h4>
-
-        {
-          <div className="p-3 text-center mb-3">
-            {d?.map((doc) => {
-              return (
-                <span className="border border-danger mx-2 p-2 bg-white">
-                  {doc.email.slice(0, -10)}
-                </span>
-              );
-            })}
-          </div>
-        }
-      </div>
       
         </div>
       )}
-
-
 
       {currentUser && (
 
@@ -633,13 +263,20 @@ export const AuctionBody = () => {
       )}
 
             <div className={admin !== "superadmin@gmail.com" && 'd-none'}>
-                {
-                   arr.filter((el) => el !== undefined)
-                   .map((el) => {
-                     return (
-                       <p>{el.tiendaOxxo} {el.completed ? '✓' : '✘'}</p>
-                     );
-                   })
+                
+                {  
+                      arr.filter(el => el !== undefined)
+                      .sort(function (o1,o2) {
+                        if (o1.tiendaOxxo > o2.tiendaOxxo) { //comparación lexicogŕafica
+                          return 1;
+                        } else if (o1.tiendaOxxo < o2.tiendaOxxo) {
+                          return -1;
+                        } 
+                        return 0;
+                      })
+                      .map((el, index) => (
+                          <List el={el} index={index} key={index}/>
+                      ))     
                 }
               
             </div>
---
+++ src/components/auctions/List.js
@@ -0,0 +1,56 @@
+import './picker.css'
+
+const list = ({ el }) => {
+
+      let color = el.tiendaOxxo.at(0)
+      let colorl = el.tiendaOxxo.at(-1)
+
+      console.log(el)
+
+
+  return (
+    <div className={
+      colorl === 'A' && 'c1' || colorl === 'B' && 'c2' || colorl === 'C' && 'c3' ||
+      colorl === 'D' && 'c4' || colorl === 'E' && 'c5' || colorl === 'F' && 'c6' ||
+      colorl === 'G' && 'c7' || colorl === 'H' && 'c8' || colorl === 'I' && 'c9' ||
+      colorl === 'J' && 'c10' || colorl === 'K' && 'c11' || colorl === 'L' && 'c12' ||
+      colorl === 'M' && 'c13' || colorl === 'N' && 'c14' || colorl === 'O' && 'c15' ||
+      colorl === 'P' && 'c16' || colorl === 'Q' && 'c17' || colorl === 'R' && 'c18' ||
+      colorl === 'S' && 'c19' || colorl === 'T' && 'c20' || colorl === 'U' && 'c21' ||
+      colorl === 'V' && 'c22' || colorl === 'W' && 'c23' || colorl === 'X' && 'c24' ||
+      colorl === 'Y' && 'c25' || colorl === 'Z' && 'c26' || colorl === '1' && 'c27' ||
+      colorl === '2' && 'c28' || colorl === '3' && 'c29' || colorl === '4' && 'c30' ||
+      colorl === '5' && 'c31' || colorl === 'c32' && 'c26' || colorl === '7' && 'c33' ||
+      colorl === '8' && 'c34' || colorl === '9' && 'c35' ||
+
+      color === 'A' && 'c1' || color === 'B' && 'c2' || color === 'C' && 'c3' ||
+      color === 'D' && 'c4' || color === 'E' && 'c5' || color === 'F' && 'c6' ||
+      color === 'G' && 'c7' || color === 'H' && 'c8' || color === 'I' && 'c9' ||
+      color === 'J' && 'c10' || color === 'K' && 'c11' || color === 'L' && 'c12' ||
+      color === 'M' && 'c13' || color === 'N' && 'c14' || color === 'O' && 'c15' ||
+      color === 'P' && 'c16' || color === 'Q' && 'c17' || color === 'R' && 'c18' ||
+      color === 'S' && 'c19' || color === 'T' && 'c20' || color === 'U' && 'c21' ||
+      color === 'V' && 'c22' || color === 'W' && 'c23' || color === 'X' && 'c24' ||
+      color === 'Y' && 'c25' || color === 'Z' && 'c26' 
+    
+    }>
+      <span style={{marginLeft:'10px'}}>
+            {el.tiendaOxxo}
+      </span>
+
+      <span className={!el.completed ? 'text-danger mx-2' : 'text-primary mx-2'}>
+       {el.completed ? "✓" : "✘"} 
+      </span> 
+
+       {el.email?.slice(0, -10)}
+    </div>
+  );
+};
+
+export default list;
+
+{
+  /* <div className={el.duration} key={el.duration}>
+
+</div> */
+}
